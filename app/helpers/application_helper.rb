9310ee3f796502d26986a372a3ab4ef48d987c81
jackal998 <jackal998@gmail.com>
2017-01-16 21:55:08 +0800

    user_name method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def user_name(user)
+    if user.nickname
+      if user.nickname.length > 14 
+        return user.nickname[0..14] + "..."
+      else
+        return user.nickname
+      end
+    else
+      return user.email.split("@").first
+    end
+  end
 end
